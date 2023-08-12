14d0ea420b28fa2ffec78977646fbc26a568a3bf
0bellum0 <fpguerra00@gmail.com>
2023-08-12 10:04:35 -0300

    Start

---
+++ Jogo da forca/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using JogoDaForca;
+
+World world = new World(20, 80);
---
+++ Jogo da forca/World.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace JogoDaForca
+{
+	internal class World
+	{
+		public int Lines;
+
+		public int Columns;
+
+		public char[,] Map;
+
+		public World(int lines, int columns)
+		{
+			Lines = lines;
+
+			Columns = columns;
+
+			Map = new char[Lines, Columns];
+		}
+
+		public void Fill()
+		{
+			for (int l = 0; l < Lines; l++)
+			{
+				for (int c = 0; c < Columns; c++)
+				{
+					Map[l, c] = 'X';
+				}
+			}
+		}
+	}
+}
