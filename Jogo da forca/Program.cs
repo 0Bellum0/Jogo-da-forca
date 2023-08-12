6271cc64b550b72899a1a5d09be2a9b22bc7f2d1
0bellum0 <fpguerra00@gmail.com>
2023-08-12 13:02:36 -0300

    commit

--- Jogo da forca/Program.cs
+++ Jogo da forca/Program.cs
@@ -2,4 +2,48 @@
 
 using JogoDaForca;
 
-World world = new World(20, 80);
+World world = new World(10, 20);
+
+string forca = @"
+_______
+|     ¿
+|
+|
+-
+";
+
+string palavra = "palavra";
+
+string descoberto = "_______";
+
+string erros = "";
+
+world.Fill();
+world.Add(2, 4, forca);
+world.Draw();
+
+
+while (true)
+{
+    world.Fill();
+    Console.WriteLine("Digite uma Letra: ");
+    string letra = Console.ReadLine();
+
+    if (letra == "0")
+    {
+        break;
+    }
+
+    if (palavra.Contains(letra))
+    {
+
+    }
+    else
+    {
+        erros += $"{letra}";
+    }
+}
+
+world.Add(2, 4, descoberto);
+world.Add(7, 4, erros);
+world.Draw();
--- Jogo da forca/World.cs
+++ Jogo da forca/World.cs
@@ -20,12 +20,83 @@ namespace JogoDaForca
 
 		public void Fill()
 		{
-			for (int l = 0; l < Lines; l++)
+            //╝ ╔ ╚ ╗ ═ ║
+
+            for (int l = 0; l < Lines; l++)
 			{
 				for (int c = 0; c < Columns; c++)
 				{
-					Map[l, c] = 'X';
+					if (l==0 && c==0)
+					{
+                        Map[l, c] = '╔';
+                    }
+					else if (l==0 && c==Columns-1)
+					{
+                        Map[l, c] = '╗';
+                    }
+					else if ((l!=0 || l!=Lines-1) && (c==0 || c==Columns-1))
+					{
+                        Map[l, c] = '║';
+
+                    }
+					else if ((l==0 || l==Lines-1) && (c!=0 || c!=Columns-1))
+					{
+                        Map[l, c] = '═';
+
+                    }
+					else if (l==Lines-1 && c==0)
+					{
+                        Map[l, c] = '╚';
+
+                    }
+					else if (l==Lines-1 && c==Columns-1)
+					{
+                        Map[l, c] = '╝';
+
+                    }
+					else
+                    {
+                        Map[l, c] = ' ';
+
+                    }
+                }
+			}
+		}
+
+		public void Draw()
+		{
+            for(int l = 0; l < Lines; l++)
+			{
+                for (int c = 0; c < Columns; c++)
+                {
+					Console.Write(Map[l, c]);
+                }
+
+				Console.WriteLine();
+            }
+        }
+
+		public void Add(int l, int c, string text)
+		{
+			string[] vet;
+
+			text = text.Trim();
+
+			vet = text.Split('\n');
+
+			int pos = c;
+
+			for (int i = 0; i < vet.Length; i++)
+			{
+				for (int j = 0; j < vet[i].Length; j++)
+				{
+					string palavra = vet[i];
+					palavra = palavra.Replace('\r', ' ');
+					Map[l, pos] = palavra[j];
+					pos++;
 				}
+				pos = c;
+				l++;
 			}
 		}
 	}
